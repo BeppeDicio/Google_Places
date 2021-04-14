ef7125f2bdad986715c3f57e6db61b3ade0201b8
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-04-14 16:22:28 +0200

    [master] init project

---
+++ places_app/places_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  places_app
+//
+//  Created by Giuseppe Diciolla on 14/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ places_app/places_app/places_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  places_appApp.swift
+//  places_app
+//
+//  Created by Giuseppe Diciolla on 14/04/21.
+//
+
+import SwiftUI
+
+@main
+struct places_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
